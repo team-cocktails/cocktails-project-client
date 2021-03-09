12a86e2667226ffd8c5f1a7d1e801df779e3fe90
Vance-M <myveryvills@yahoo.com>
2021-03-09 10:12:54 -0800

    set up routes and file structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router, 
+  Route, 
+  Switch,
+  PrivateRoute,
+} from 'react-router-dom';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import HomePage from './Homepage/HomePage.js';
+import DetailPage from './Searchpage/DetailPage.js';
+import SignUpPage from './Auth/SignUpPage.js';
+import LoginPage from './Auth/LoginPage.js';
+import MenuPage from './Searchpage/MenuPage.js';
+import AboutPage from './components/AboutPage.js';
+import SearchPage from './Searchpage/SearchPage.js';
+export default class App extends Component {
+  render() {
+      return (
+          <div>
+              <Router>
+                  <Switch>
+                      <Route 
+                          path="/" 
+                          exact
+                          render={(routerProps) => <HomePage {...routerProps} />} 
+                      />
+                      <Route 
+                          path="/detail" 
+                          exact
+                          render={(routerProps) => <DetailPage {...routerProps} />} 
+                      />
+                      <Route 
+                        path="/signup" 
+                        exact
+                        render={(routerProps) => <SignUpPage {...routerProps} />} 
+                      />
+                      <Route 
+                          path="/login" 
+                          exact
+                          render={(routerProps) => <LoginPage {...routerProps} />} 
+                      />
+                      <PrivateRoute 
+                            path="/menu" 
+                            exact
+                            token={this.state.user && this.state.user.token}
+                            render={(routerProps) => 
+                              <MenuPage 
+                                user={this.state.user}
+                                {...routerProps} 
+                              />} 
+                        />
+                      <Route 
+                          path="/about" 
+                          exact
+                          render={(routerProps) => <AboutPage {...routerProps} />} 
+                      />
+                      <Route 
+                          path="/search" 
+                          exact
+                          render={(routerProps) => <SearchPage {...routerProps} />} 
+                      />
+                  </Switch>
+              </Router>
+          </div>
+      )
+  }
 }
 
-export default App;

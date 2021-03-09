dcd50a7c74b5a47fba9820f49508bfae62bbb614
taylorreichner <taylorreichner@yahoo.com>
2021-03-09 11:54:49 -0800

    added signin and login page

--- src/Auth/SignUpPage.js
+++ src/Auth/SignUpPage.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react'
+import { signUpUser } from '../components/Utils.js';
 
 export default class SignUpPage extends Component {
+
+    state = {
+        email: '',
+        password: '',    
+    }
+
+    handleEmailChange = (e) => this.setState({ email: e.target.value});
+
+    handlePasswordChange = (e) => this.setState({ password: e.target.value});
+
+    handleSubmit = async e => {
+        e.preventDefault()
+    
+        try {
+            const user = await signUpUser(this.state.email, this.state.password);
+
+            this.props.handleUserChange(user);
+
+            this.props.history.push('/search');
+
+        } catch(e) {
+            this.setState({ error: e.response.body.error })
+        }
+    }
+    
     render() {
         return (
             <div>
-                
+                <h2>SIGNUP</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Email
+                            <input type="email" value={this.state.email} onChange={this.handleEmailChange} />
+                    </label>
+                    <label>
+                        Password
+                            <input value={this.state.password} onChange={this.handlePasswordChange} />
+                    </label>
+                    <button>Submit</button>
+                </form>
             </div>
         )
     }
 }
+

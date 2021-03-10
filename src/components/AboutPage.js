ffe04328cb50f37af095bed6ebf2605370eed509
JosieMCantu <josiex2@gmail.com>
2021-03-10 15:43:00 -0800

    adds about page styles

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react'
 import josie from '../assets/josie.JPEG';
+import vance from '../assets/vance.jpeg';
+import stephen from '../assets/stephen.png';
+import taylor from '../assets/taylor.jpg';
 
 
 export default class AboutPage extends Component {
     render() {
         return (
-            <>
-            <h1>About us page</h1>
+            <div className='members-container'>
             
-            <div className='members'>
-                
-                <div><img alt='josie dark bar' src={josie} /></div>
-                <div><img alt='josie dark bar' src={josie} /></div>
-                <div><img alt='josie dark bar' src={josie} /></div>
-                <div><img alt='josie dark bar' src={josie} /></div>
-                
+                <h1 className='members-header'>Meet the team at What'll ya have? </h1>
+                <div className='members'>
+                    <div className='members-item'>
+                        <img alt='Vance drinking' src={vance} />
+                        <div>Software Engineer</div>
+                        <div>Favorite Bar: RSL Reno, NV</div>
+                        <div>Favorite Cocktail: Dark &#38; Stormy</div>
+                    </div>
+                    <div className='members-item'>
+                        <img alt='josie drinking' src={josie} />
+                        <div>Software Engineer</div>
+                        <div>Favorite Bar: The Saphire Hotel Portland, OR</div>
+                        <div>Favorite Cocktail: French 75</div>
+                    </div>
+                    <div className='members-item'>
+                        <img alt='Stephen drinking' src={stephen} />
+                        <div>Software Engineer</div>
+                        <div>Favorite Bar: Cat's Paw Saloon Portland, OR</div>
+                        <div>Favorite Cocktail: Negroni</div>
+                    </div>
+                    <div className='members-item'>
+                        <img alt='Taylor drinking' src={taylor} />
+                        <div>Software Engineer</div>
+                        <div>Favorite Bar: Triple Nickel Portland, OR</div>
+                        <div>Favorite Cocktail: Moscow Mule</div>
+                    </div>
+                </div>
             </div>
-            </>
         )
     }
 }

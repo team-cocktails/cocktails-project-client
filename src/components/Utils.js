c55c4931a0ccbcf1186af4aa09b7a4258e8305cf
taylorreichner <taylorreichner@yahoo.com>
2021-03-10 13:46:22 -0800

    port issue

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -1,6 +1,6 @@
 import request from "superagent";
 
- const URL = 'http://localhost:3009';
+ const URL = 'http://localhost:300';
 // const URL = `https://cocktails-project-server.herokuapp.com`;
 
 export async function signUpUser(email, password) {

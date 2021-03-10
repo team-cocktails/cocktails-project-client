fdbfbf31e030ab93adab48e262fa8c1d71ca1bcc
JosieMCantu <josiex2@gmail.com>
2021-03-10 14:27:45 -0800

    adds homepage styles and header switch

--- src/Homepage/HomePage.js
+++ src/Homepage/HomePage.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
-
+import HomeButtons from '../components/HomeButtons.js';
 import bar from '../assets/bar.jpg';
 
 export default class HomePage extends Component {
     render() {
         return (
-            <div className='home-container'>
-                {/* <div className='header-words'>What'll ya have?</div> */}
-                <div><img className='home' src={bar} alt={'pictures'} /></div>
-                <div className='home-words'>Are you over 21?</div>
-                
-            </div>
+            <>
+                <div className='home-header'>What'll Ya Have?</div>
+                <div className='home-container'>
+                    <img className='home-img' src={bar} alt={'pictures'} />
+                    <div className='home-words'>First things first, are you over 21?
+                    <HomeButtons /></div>
+                    
+                </div>
+            </>
         )
     }
 }

a48201bffc3d6364944b9aed022fad1288faed98
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-03-11 13:51:40 -0800

    fix router again

--- src/components/HomeButtons.js
+++ src/components/HomeButtons.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
 export default class HomeButtons extends Component {
-    
+
 
     render() {
         return (
             <div>
-                <Link to={`./signup`}><button className='yes-button'>Yes</button></Link>
-                <a href='https://www.google.com'><button className='no-button'>No</button></a>
+                <Link to={`./signup`}><button className='home-button'>Yes</button></Link>
+                <a href='https://www.google.com'><button className='home-button'>No</button></a>
             </div>
         )
     }
